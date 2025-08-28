63734cbe30ca9f77f73d0bc454c40e0d6ae40b6e
itsbishal10 <bishalsharma7777@gmail.com>
2025-08-28 20:15:03 +0530

    Updated basic CRUD

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,17 @@
         <button>Log out</button>
 
     </form>
+    <div style="border: 3px solid black;">
+    <h2>Create a new Post </h2>    
+    <form action="/create-post" method="POST">
+    @csrf
+    <input type="text" name="title" placeholder="post title">
+    <textarea name="body" placeholder="write the post here"></textarea>
+    <button>Create Post</button>
+    </form>
+    </div>
+
+    
     @else   
 
     <div style="border: 3px solid black;">
@@ -38,7 +49,21 @@
     </div>
 
     @endauth
-
+  </br>
+    <div style="border: 3px solid black;">
+    <h2>All Posts</h2>
+    @foreach ($posts as $post)
+        <div style="background-color:skyblue; padding:10px; margin: 10px;">
+          <h3>{{$post['title']}} by {{$post->user->name}}</h3>
+          {{$post['body']}}
+          <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
+          <form action="/delete-post/{{$post->id}}" method="POST">
+          @csrf
+          @method('DELETE')
+          <button>Delete Post</button>
+        </form>
+        </div>
+    @endforeach
 
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,31 @@
 <?php
 
-use App\Http\Controllers\userController;
+
+use App\Models\Post;
+use App\Models\User as AppUser;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\userController;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::get('/', function () {
-    return view('home');
+    $user = Auth::user();
+    if ($user instanceof AppUser) {
+        $posts = $user->userCoolPosts()->latest()->get();
+    } else {
+        $posts = Post::latest()->get();
+    }
+    return view('home', ['posts' => $posts]);
 });
 
 Route::post('/register', [userController::class, 'register']);
 Route::post('/logout', [userController::class, 'logout']);
 Route::post('/login', [userController::class, 'login']);
+
+
+//Blog post related routes
+Route::post('/create-post', [PostController::class, 'createPost'])->middleware('auth');
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'delete'])->middleware('auth');
